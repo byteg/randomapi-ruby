f88d6f446ef97b3105b3f9a23a715ab8bcab1bb3
Denis Kuznetsov <denis@f7.ru>
2015-02-09 12:16:20 +0300

    removed client/user, all code moved to Client

--- lib/randomapi/client.rb
+++ lib/randomapi/client.rb
@@ -1,7 +1,9 @@
 module RandomApi
   class Client < API
-    Dir[File.expand_path('../client/*.rb', __FILE__)].each{|f| require f}
-    include RandomApi::Client::User
+
+    def request(options = {})
+      get("", options)
+    end
 
   end
 end
