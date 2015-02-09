e91c32e6807098a43b82df134c9ccc0919f05275
Denis Kuznetsov <denis@f7.ru>
2015-02-09 12:17:54 +0300

    renamed private request => make_request

--- lib/randomapi/request.rb
+++ lib/randomapi/request.rb
@@ -5,28 +5,28 @@ module RandomApi
   module Request
     # Perform an HTTP GET request
     def get(path, options={}, raw=false)
-      request(:get, path, options, raw)
+      make_request(:get, path, options, raw)
     end
 
     # Perform an HTTP POST request
     def post(path, options={}, raw=false)
-      request(:post, path, options, raw)
+      make_request(:post, path, options, raw)
     end
 
     # Perform an HTTP PUT request
     def put(path, options={}, raw=false)
-      request(:put, path, options, raw)
+      make_request(:put, path, options, raw)
     end
 
     # Perform an HTTP DELETE request
     def delete(path, options={}, raw=false)
-      request(:delete, path, options, raw)
+      make_request(:delete, path, options, raw)
     end
 
     private
 
     # Perform an HTTP request
-    def request(method, path, options, raw=false)
+    def make_request(method, path, options, raw=false)
       options.merge!({:key => api_key, :id => api_id })
 
       response = connection(raw).send(method) do |request|
